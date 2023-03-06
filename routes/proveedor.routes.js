d9d9e5d96cbd038fcf701a9f69ef6e04dc66c6e2
Javier Elizondo <josejavierem@gmail.com>
2023-03-06 17:37:59 -0600

    Update Proveedores
    Sección para registrar proveedores terminada

--- routes/proveedor.routes.js
+++ routes/proveedor.routes.js
@@ -1,4 +1,56 @@
 const express = require('express')
 const routes = express.Router()
 
+//Obtener todos los proveedores
+routes.get('/proveedor', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('SELECT * FROM proveedor', (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.json(rows)
+        })
+    })
+})
+
+//Ingresar un proveedor
+routes.post('/proveedor', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('INSERT INTO proveedor set ?', [req.body], (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('Proveedor registrado!')
+        })
+    })
+})
+
+//Borrar un proveedor
+routes.delete('/proveedor/:id', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('DELETE FROM proveedor WHERE id = ?', [req.params.id], (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('Provedor eliminado!')
+        })
+    })
+})
+
+//Actualizar un proveedor
+routes.put('/proveedor/:id', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('UPDATE proveedor set ? WHERE id = ?', [req.body, req.params.id], (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('proveedor actualizado!')
+        })
+    })
+})
+
 module.exports = routes
